5daf5aa154869b8b4d8869121399accc2cd60de5
Arman1407 <131908661+Arman1407@users.noreply.github.com>
2023-06-17 16:01:29 +0300

    add task#1

--- additional task#1/Program.cs
+++ additional task#1/Program.cs
@@ -17,52 +17,48 @@
 //                  6 2 4 3                 6 2 4 3
 //                  1 2 8 7	                5 9 2 6
 
-
 void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-        matrix[i, j] = new Random().Next(1, 31); // [1; 30]
+            matrix[i, j] = new Random().Next(1, 31); // [1; 30]
     }
 }
+
 
 void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-        Console.Write($"{matrix[i, j]} \t");
+            Console.Write($"{matrix[i, j]} \t");
         Console.WriteLine();
     }
 }
 
+
 void ReverseMatrix(int[,] matrix)
 {
-        for(int j = 0; j < matrix.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            int temp = matrix[0, j];
-            matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
-            matrix[matrix.GetLength(0) - 1, j] = temp;
-            // matrix[0, j] ++;
-            // for(int i = 0; i < matrix.GetLength(1); i++)
-            // {
-            //     int temp1 = matrix[1, j];
-            //     matrix[1, j] = matrix[matrix.GetLength(0) - 2, j];
-            //     matrix[matrix.GetLength(0) - 2, j] = temp1;
-            // } 
-
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[matrix.GetLength(0) - 1 - i, j];
+            matrix[matrix.GetLength(0) - 1 - i, j] = temp;
         }
-
+    }
 }
+
 
 Console.Clear();
 Console.Write("Введите размер массива: ");
 int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
+Console.WriteLine("___________________________");
 PrintMatrix(matrix);
-Console.WriteLine();
+Console.WriteLine("___________________________");
 ReverseMatrix(matrix);
 PrintMatrix(matrix);
- // Как перевернуть весь массив, а не только первую и последнюю строкку?
